2e0c2155a4fff818158239955b5a8a5ae7b160bd
Aliyaqara Estatika <ayaa.qor@gmail.com>
2014-09-25 13:16:02 +0700

    dirapihin
    dirapihin dulu ya piiik smaa aku biar ga pusing liat kodingannya eheheh

--- BilanganGenap20.java
+++ BilanganGenap20.java
@@ -4,9 +4,12 @@ public class BilanganGenap20
     public static void main(String[]args)
 	{
 		int bil;
-        for(bil=1;bil<=20;bil++){
-		if(bil%2==0){
-		System.out.print(bil+",");}
-            }
-}
+        	for(bil=1;bil<=20;bil++)
+        	{
+			if(bil%2==0)
+			{
+			System.out.print(bil+",");
+			}
+		}
+	}
 }
