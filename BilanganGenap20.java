2ee97fd18ccd5f7e380c81b4e19a42415a991ce4
devikasilpiana <devika_silpiana@yahoo.com>
2014-09-24 22:57:48 -0700

    tugas pertama

---
+++ BilanganGenap20.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BilanganGenap20 
+{
+    public static void main(String[]args)
+	{
+		int bil;
+        for(bil=1;bil<=20;bil++){
+		if(bil%2==0){
+		System.out.print(bil+",");}
+            }
+}
+}
